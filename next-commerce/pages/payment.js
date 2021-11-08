3b1dcf07f6357b986b3d64ec5d5dbb21327b2b80
Arvind Shelke <ucdonor5@gmail.com>
2021-11-08 15:11:40 +0530

    shipping page, shippingAddress cookie

---
+++ next-commerce/pages/payment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Payment() {
+  return (
+    <div>
+      <h1>payments</h1>
+    </div>
+  )
+}
--- next-commerce/utils/Store.js
+++ next-commerce/utils/Store.js
@@ -8,10 +8,12 @@ const initialState = {
     cartItems: Cookies.get("cartItems")
       ? JSON.parse(Cookies.get("cartItems"))
       : [],
+    shippingAddress: Cookies.get("shippingAddress")
+      ? JSON.parse(Cookies.get("shippingAddress"))
+      : {},
   },
   userInfo: Cookies.get("userInfo")
     ? JSON.parse(Cookies.get("userInfo"))
-    // Cookies.get("userInfo")
     : null,
 };
 
@@ -42,11 +44,16 @@ function reducer(state, action) {
       Cookies.set("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case "USER_LOGIN": {
+    case "USER_LOGIN":
       return { ...state, userInfo: action.payload };
-    }
+      
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      };
     case "USER_LOGOUT": {
-      return { ...state, userInfo: null, cart:{cartItems:[]} };
+      return { ...state, userInfo: null, cart: { cartItems: [] } };
     }
 
     default:
